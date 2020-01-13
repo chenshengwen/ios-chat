b39e88a9195369e356e57f1388883b64f10d5326
chensw <605975666@qq.com>
2020-01-13 22:57:22 +0800

    修复群私聊关闭频繁提示问题

--- wfclient/WFChatClient/Messages/WFCCGroupPrivateChatNotificationContent.m
+++ wfclient/WFChatClient/Messages/WFCCGroupPrivateChatNotificationContent.m
@@ -11,6 +11,26 @@
 #import "WFCCNetworkService.h"
 #import "Common.h"
 
+#define kAccountUserDefault(key) [NSString stringWithFormat:@"%@_%@_%@",key,[WFCCNetworkService sharedInstance].userId,self.groupId]
+
+#define kUserDefault [NSUserDefaults standardUserDefaults]
+
+static NSString *const kcreatorY = @"kcreatorY";
+static NSString *const kcreatorN = @"kcreatorN";
+
+static NSString *const kfriendAliasY = @"kfriendAliasY";
+static NSString *const kfriendAliasN = @"kfriendAliasN";
+
+static NSString *const kgroupAliasY = @"kgroupAliasY";
+static NSString *const kgroupAliasN = @"kgroupAliasN";
+
+static NSString *const kdisplayNameY = @"kdisplayNameY";
+static NSString *const kdisplayNameN = @"kdisplayNameN";
+
+static NSString *const kotherY = @"kotherY";
+static NSString *const kotherN = @"kotherN";
+
+
 @implementation WFCCGroupPrivateChatNotificationContent
 - (WFCCMessagePayload *)encode {
     WFCCMessagePayload *payload = [super encode];
@@ -68,16 +88,82 @@
 
 - (NSString *)formatNotification:(WFCCMessage *)message {
     if ([[WFCCNetworkService sharedInstance].userId isEqualToString:self.creator]) {
+        
+        if ([self.type isEqualToString:@"0"]) {
+            if (![kUserDefault boolForKey:kAccountUserDefault(kcreatorY)]) {
+                [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kAccountUserDefault(kcreatorY)];
+                [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kAccountUserDefault(kcreatorN)];
+                return @"你开启了成员私聊";
+            }
+        }else {
+            if (![kUserDefault boolForKey:kAccountUserDefault(kcreatorN)]) {
+                 [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kAccountUserDefault(kcreatorY)];
+                 [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kAccountUserDefault(kcreatorN)];
+                return @"你关闭了成员私聊";
+            }
+        }
         return [self.type isEqualToString:@"0"] ? @"你开启了成员私聊" : @"你关闭了成员私聊";
     } else {
         WFCCUserInfo *userInfo = [[WFCCIMService sharedWFCIMService] getUserInfo:self.creator inGroup:self.groupId refresh:NO];
         if (userInfo.friendAlias.length > 0) {
+            if ([self.type isEqualToString:@"0"]) {
+               if (![kUserDefault boolForKey:kAccountUserDefault(kfriendAliasY)]) {
+                   [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kAccountUserDefault(kfriendAliasY)];
+                   [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kAccountUserDefault(kfriendAliasN)];
+                   return [NSString stringWithFormat:[self.type isEqualToString:@"0"] ? @"%@开启了成员私聊" : @"%@关闭了成员私聊", userInfo.friendAlias];
+               }
+           }else {
+               if (![kUserDefault boolForKey:kAccountUserDefault(kfriendAliasN)]) {
+                    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kAccountUserDefault(kfriendAliasY)];
+                    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kAccountUserDefault(kfriendAliasN)];
+                   return [NSString stringWithFormat:[self.type isEqualToString:@"0"] ? @"%@开启了成员私聊" : @"%@关闭了成员私聊", userInfo.friendAlias];
+               }
+           }
             return [NSString stringWithFormat:[self.type isEqualToString:@"0"] ? @"%@开启了成员私聊" : @"%@关闭了成员私聊", userInfo.friendAlias];
         } else if(userInfo.groupAlias.length > 0) {
+             if ([self.type isEqualToString:@"0"]) {
+                if (![kUserDefault boolForKey:kAccountUserDefault(kgroupAliasY)]) {
+                    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kAccountUserDefault(kgroupAliasY)];
+                    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kAccountUserDefault(kgroupAliasN)];
+                    return [NSString stringWithFormat:[self.type isEqualToString:@"0"] ? @"%@开启了成员私聊" : @"%@关闭了成员私聊", userInfo.groupAlias];
+                }
+            }else {
+                if (![kUserDefault boolForKey:kAccountUserDefault(kgroupAliasN)]) {
+                     [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kAccountUserDefault(kgroupAliasY)];
+                     [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kAccountUserDefault(kgroupAliasN)];
+                    return [NSString stringWithFormat:[self.type isEqualToString:@"0"] ? @"%@开启了成员私聊" : @"%@关闭了成员私聊", userInfo.groupAlias];
+                }
+            }
             return [NSString stringWithFormat:[self.type isEqualToString:@"0"] ? @"%@开启了成员私聊" : @"%@关闭了成员私聊", userInfo.groupAlias];
         } else if (userInfo.displayName.length > 0) {
+            if ([self.type isEqualToString:@"0"]) {
+                if (![kUserDefault boolForKey:kAccountUserDefault(kdisplayNameY)]) {
+                    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kAccountUserDefault(kdisplayNameY)];
+                    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kAccountUserDefault(kdisplayNameN)];
+                    return [NSString stringWithFormat:[self.type isEqualToString:@"0"] ? @"%@开启了成员私聊" : @"%@关闭了成员私聊", userInfo.displayName];
+                }
+            }else {
+                if (![kUserDefault boolForKey:kAccountUserDefault(kdisplayNameN)]) {
+                     [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kAccountUserDefault(kdisplayNameY)];
+                     [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kAccountUserDefault(kdisplayNameN)];
+                    return [NSString stringWithFormat:[self.type isEqualToString:@"0"] ? @"%@开启了成员私聊" : @"%@关闭了成员私聊", userInfo.displayName];
+                }
+            }
             return [NSString stringWithFormat:[self.type isEqualToString:@"0"] ? @"%@开启了成员私聊" : @"%@关闭了成员私聊", userInfo.displayName];
         } else {
+            if ([self.type isEqualToString:@"0"]) {
+               if (![kUserDefault boolForKey:kAccountUserDefault(kotherY)]) {
+                   [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kAccountUserDefault(kotherY)];
+                   [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kAccountUserDefault(kotherN)];
+                   return [NSString stringWithFormat:[self.type isEqualToString:@"0"] ? @"用户<%@>开启了成员私聊" : @"用户<%@>关闭了成员私聊", self.creator];
+               }
+           }else {
+               if (![kUserDefault boolForKey:kAccountUserDefault(kotherN)]) {
+                    [[NSUserDefaults standardUserDefaults] setBool:NO forKey:kAccountUserDefault(kotherY)];
+                    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kAccountUserDefault(kotherN)];
+                   return [NSString stringWithFormat:[self.type isEqualToString:@"0"] ? @"用户<%@>开启了成员私聊" : @"用户<%@>关闭了成员私聊", self.creator];
+               }
+           }
             return [NSString stringWithFormat:[self.type isEqualToString:@"0"] ? @"用户<%@>开启了成员私聊" : @"用户<%@>关闭了成员私聊", self.creator];
         }
     }
